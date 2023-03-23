66dc68b4054edf4c49f4e21dfebcbafe6aa1e0dd
Vinicius <vinicius.am.souza221b@gmail.com>
2023-03-22 23:39:24 -0300

    Criação da model de produto e categoria, retornando todos os produtos na funcao index

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProdutoController::class, 'index']);

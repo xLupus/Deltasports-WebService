91b8bbde3981b46ef9f91f6a65ab5aadbfeb520e
Vinicius <vinicius.am.souza221b@gmail.com>
2023-04-16 02:19:31 -0300

    Filtro, Ordenação e Paginação de Produtos

--- app/Http/Controllers/Api/ProdutoController.php
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -6,21 +6,100 @@ use App\Http\Resources\Api\ProdutoResource;
 use Illuminate\Http\Request;
 use App\Models\Produto;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 
 class ProdutoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $products = Produto::ativos()->get();
+        $query = Produto::ativos();
 
-        return response()->json([
-            "status"    => 200,
-            "message"   => "Todos os produtos",
-            "products"  => ProdutoResource::collection($products)
-        ]);
+        $statusMessage = 0;
+        $message = 'Todos os Produtos';
+        $products = [];
+
+        $filterQuery = $request->input('filter', null);
+
+        if ($filterQuery) {
+            $filterAcceptColumns = ['categoria'];
+
+            [$filterColumn, $filterParam] = explode(':', $filterQuery);
+
+            if (in_array($filterColumn, $filterAcceptColumns)) {
+                if ($filterColumn == 'categoria') {
+                    $query->whereRelation('categoria', 'CATEGORIA_NOME', '=', $filterParam);
+
+                    $statusMessage = 200;
+                }
+            } else {
+                // TODO - Qual a regra de negocio nesse caso
+            }
+        }
+
+        $orderQuery = $request->input('order', null);
+
+        if ($orderQuery) {
+            $orderAcceptColumns = ['categoria', 'nome', 'preco'];
+
+            $sorts = explode(',', $orderQuery);
+
+            foreach ($sorts as $sortColumn) {
+                $sortDirection = $orderQuery[0] == '-' ? 'DESC' : 'ASC';
+
+                $sortColumn = ltrim($sortColumn, '-'); //retira espaco em branco ou outro caracter do comeco da string 
+
+                if (in_array($sortColumn, $orderAcceptColumns)) {
+                    if ($sortColumn == 'nome') {
+                        $query->orderBy('PRODUTO_NOME', $sortDirection);
+                    }
+
+                    if ($sortColumn == 'preco') {
+                        $query->orderBy('PRODUTO_PRECO', $sortDirection);
+                    }
+
+                    if ($sortColumn == 'categoria') {
+                        // TODO
+                    }
+                } else {
+                    // TODO - Qual a regra de negocio nesse caso
+                }
+            }
+        }
+
+        $page = $request->input('page', 1); // TODO - Validar se é numero 
+        $productsPerPage = 10;
+        
+        try {
+            $query->offset(($page - 1) * $productsPerPage)->limit($productsPerPage);
+            $products = $query->get();
+
+            $numberOfProducts = $products->count();
+            $numberOfPages = ceil($numberOfProducts / $productsPerPage);
+    
+            $meta = [
+                'numberOfPages'    => $numberOfPages,
+                'numberOfProducts' => $numberOfProducts,
+                'currentPage'      => $page,
+                'productsPerPage'  => $productsPerPage
+            ];
+    
+            return response()->json([
+                "status"    => $statusMessage,
+                "message"   => "Todos os produtos",
+                "meta"      => $meta,
+                "products"  => ProdutoResource::collection($products)
+            ]);
+        } catch (\Exception $err) {
+            $class = get_class($err);
+
+            return response()->json([
+                "status"    => 500,
+                "message"   => "Ops! Ocorreu um erro, por favor tente novamente."
+            ]);
+        }        
     }
 
 
@@ -32,7 +111,7 @@ class ProdutoController extends Controller
         $productId = $request->id;
 
         $product = Produto::ativos()->where('PRODUTO_ID', $productId)
-                                    ->get();
+            ->get();
 
         return response()->json([
             "status"    => 200,
@@ -44,8 +123,8 @@ class ProdutoController extends Controller
     public function search(Request $request)
     {
         $query = $request->name;
-        $produto = Produto::ativos()->where('PRODUTO_NOME', 'like', '%'.$query.'%')->get();
-        return response()->json(['Produtos'=>$produto],200);
+        $produto = Produto::ativos()->where('PRODUTO_NOME', 'like', '%' . $query . '%')->get();
+        return response()->json(['Produtos' => $produto], 200);
         // return view('produto.search', ['produto' => $produto]);
     }
 }

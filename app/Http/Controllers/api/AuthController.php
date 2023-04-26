91346b0d5a43f8f3319cfaabe00a163805421e90
Mr.R4F <rafael.soares0777@gmail.com>
2023-04-26 17:53:54 -0300

    Mudanças na parte de autenticação: Pequenas mudanças de métodos e validação por exceções

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function __construct() {
-        $this->middleware('auth:api', ['except' => ['register', 'login', 'logout']]); //middleware de autenticação
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('register', 'login');
     }
 
     public function register(Request $request) {
@@ -24,24 +25,18 @@ class AuthController extends Controller
                 'password_confirmation' => 'required|string|min:8',
                 'cpf'                   => 'required|string|digits_between:11,11|unique:USUARIO,USUARIO_CPF'
             ], [
-                'max'                   => 'Máximo de caracteres excedido',
+                'max'                   => 'O Máximo de caracteres foi excedido',
                 'required'              => 'Preencha este campo.',
-                'not_regex'             => 'O campo informado não aceita números e símbolos.',
+                'not_regex'             => 'O campo informado não aceita números e/ ou símbolos.',
                 'min'                   => 'O campo informado deve ter no mínimo 8 digitos.',
                 'confirmed'             => 'As senhas informadas não correspondem.',
-                'email'                 => 'Formato de E-mail inválido.',
+                'email'                 => 'O formato de E-mail  é inválido.',
                 'email.unique'          => 'O E-mail informado já existe.',
                 'cpf.unique'            => 'O CPF informado já existe.',
                 'cpf.digits_between'    => 'O campo informado deve ter 11 digitos.',
             ]);
-    
-            if($validator->fails()) {
-                return response()->json([
-                    'status' => 500,
-                    'message' => $validator->messages(),
-                    'data' => null
-                ], 500);
-            }
+
+            if($validator->fails()) throw new ValidationException($validator);
 
             $user = User::create([
                 'USUARIO_NOME'  => $request['name'],
@@ -56,11 +51,7 @@ class AuthController extends Controller
                 'data' => $user
             ], 200);
         } catch (\Throwable $err) {
-            return response()->json([
-                'status' => 500,
-                'message' => 'Erro ao cadastrar usuário!',
-                'data' => null
-            ], 500);
+            return $this->exceptions($err, $validator);
         }
     }
 
@@ -74,49 +65,37 @@ class AuthController extends Controller
                 'email'     => 'Formato de E-mail inválido.',
                 'min'       => 'O campo informado deve ter no mínimo 8 digitos.',
             ]);
-    
-            if($validator->fails()) {
-                return response()->json([
-                    'status' => 500,
-                    'message' => $validator->messages(),
-                    'data' => null
-                ], 500);
-            }
-    
+
+            if($validator->fails()) throw new ValidationException($validator);
+
             $data = $request->only('email', 'password'); //mostra os dados
             $credentials = ['USUARIO_EMAIL' => $request['email'], 'password' => $request['password']]; //nome da coluna aqui deve ser 'password' para funcionar (**MUDAR APENAS NA MODEL**)
-    
-            if(! $token = Auth::attempt($credentials)) {
+
+            if($token = auth()->attempt($credentials)) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Usuário logado com sucesso!',
+                    'data' => auth()->user(),
+                    'authorization' => [
+                        'token' => $token,
+                        'type' => 'bearer',
+                    ]
+                ], 200);
+            } else {
                 return response()->json([
                     'status'    => 401,
                     'message'   => 'Email ou senha incorretos.',
                     'data'      => $data
                 ], 401);
             }
-            
-            $user = Auth::user();
-
-            return response()->json([
-                'status' => 200,
-                'message' => 'Usuário logado com sucesso!',
-                'data' => $user,
-                'authorisation' => [
-                    'token' => $token,
-                    'type' => 'bearer',
-                ]
-            ], 200);
         } catch (\Throwable $err) {
-            return response()->json([
-                'status'    => 500,
-                'message'   => 'Falha ao logar.',
-                'data'      => $data
-            ], 500);
-        }    
+            return $this->exceptions($err, $validator);
+        }
     }
 
     public function logout() {
         try { //com o token
-            $user = Auth::logout();
+            $user = auth()->logout();
 
             return response()->json([
                 'status'    => 200,
@@ -124,23 +103,64 @@ class AuthController extends Controller
                 'data'      => $user
             ], 200);
         } catch (\Throwable $err) {
-            return response()->json([
-                'status'    => 500,
-                'message'   => 'Erro ao deslogar usuário.',
-                'data'      => null
-            ], 500);
+            $this->exceptions($err);
         }
     }
 
     public function refresh() {
         return response()->json([
-            'status' => 200,
-            'message' => 'Token revalidado com sucesso!',
-            'data' => Auth::user(),
-            'authorisation' => [
-                'token' => Auth::refresh(),
-                'type' => 'bearer',
+            'status'    => 200,
+            'message'   => 'Token revalidado com sucesso!',
+            'data'      => auth()->user(),
+            'authorization' => [
+                'token'     => auth()->refresh(),
+                'type'      => 'bearer',
             ]
         ], 200);
+    }
+
+    public function user() {
+        return response()->json([
+            'status'    => 200,
+            'message'   => 'Retornado usuário' ,
+            'data'      => auth()->user()
+        ], 200);
+    }
+
+    //Exceptions
+    public function exceptions($err, $validator = null) {
+        switch (get_class($err)) {
+            case \Illuminate\Database\Eloquent\ModelNotFoundException::class:
+                return response()->json([
+                    'status' => 404,
+                    'message' => $err->getMessage(),
+                    'data' => null
+                ], 404);
+                break;
+
+            case \Illuminate\Database\QueryException::class:
+                return response()->json([
+                    'status' => 500,
+                    'message' => $err->getMessage(),
+                    'data' => null
+                ], 500);
+                break;
+
+            case \Illuminate\Validation\ValidationException::class:
+                return response()->json([
+                    'status' => 500,
+                    'message' => $validator->errors(),
+                    'data' => null
+                ], 500);
+                break;
+
+            default:
+                return response()->json([
+                    'status' => 500,
+                    'mensage' => 'Erro interno',
+                    'data' => null
+                ], 500);
+                break;
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,19 @@ use App\Http\Controllers\Api\CategoriaController;
 Route::controller(AuthController::class)->group(function () {
     Route::post('auth/register', 'register');
     Route::post('auth/login', 'login');
-    Route::post('auth/logout', 'logout');
+    Route::get('auth/logout', 'logout');
+
     Route::post('auth/refresh', 'refresh');
-});
+    Route::get('auth/user', 'user');
 
-Route::controller(ProdutoController::class)->group(function () {
-    Route::get('/products', 'index');
-    Route::get('/product/{id}', 'show');
-    Route::get('/product/search/{name}', 'search');
-});
+    Route::controller(ProdutoController::class)->group(function () {
+        Route::get('/products', 'index');
+        Route::get('/product/{id}', 'show');
+        Route::get('/product/search/{name}', 'search');
+    });
 
-Route::controller(CategoriaController::class)->group(function () {
-    Route::get('/categories', 'index');
-    Route::get('/category/{id}/products', 'showProducts');
+    Route::controller(CategoriaController::class)->group(function () {
+        Route::get('/categories', 'index');
+        Route::get('/category/{id}/products', 'showProducts');
+    });
 });
